7af8e3587a57b4b84d47a1b04fc4516d4fbdc1da
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-15 11:50:21 -0500

    add mutations in dependence module

---
+++ src/store/modules/dependence/mutations.js
@@ -0,0 +1,16 @@
+export const GET_DEPENDENCES = (state, data) => {
+  state.dependences = data
+}
+
+export const ADD_DEPENDENCE = (state, data) => {
+  state.dependences.push(data)
+}
+
+export const EDIT_DEPENDENCE = (state, data, index) => {
+  state.dependenceDraft = data
+  state.dependenceIndex = index
+}
+
+export const UPDATE_DEPENDENCE = (state, data, index) => {
+  state.dependences.splice(index, 1, data)
+}
