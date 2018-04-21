2751a9ff9727c7608eb68bb384c7cc0af77075f4
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-20 22:11:49 -0500

    add user mutation in modules

--- src/store/modules/user/mutations.js
+++ src/store/modules/user/mutations.js
@@ -1,9 +1,32 @@
-export const ADD =  (state, user) =>  {
+export const ADD_USER = (state, user) => {
+  localStorage.setItem('user', JSON.stringify(user))
   state.user = user
-},
-export const EDIT = (state, data) => {
+}
+export const ADD_USER = (state, user) => {
+  localStorage.setItem('user', JSON.stringify(user))
+  state.user = user
+}
+export const ADD_USERS = (state, users) => {
+  state.allUsers = users
+  users.map((el, index) => {
+    if (el.status === 'true') {
+      el.dependence = el.dependence.acronym
+      state.users.push(el)
+    }
+  })
+}
+export const GET_SOLICITUDES = (state) => {
+  state.solicitudes = []
+  state.allUsers.map((el) => {
+    if (el.status === 'false') {
+      state.solicitudes.push(el)
+    }
+  })
+}
+export const EDIT_USER = (state, data) => {
   state.userDraft = data
-},
+}
 export const LOGOUT = (state) => {
+  localStorage.removeItem('user')
   state.user = {}
 }
