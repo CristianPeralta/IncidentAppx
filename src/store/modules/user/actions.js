baa393e4fd80846412b93f5995f1c7662da365d7
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-20 22:17:18 -0500

    add actions in user modules

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,31 +1,69 @@
-export const getUser = ({commit, state}) => {
-  if (!state.user) {
-    LocalServices.user().then(({data}) => {
-      commit('ADD', data)
-    }).catch(() => {
-      this.$router.push({name: 'Login'})
-    })
-  }
+export const signup = ({commit}, form) => {
+  ApiServices.signup(form).then(({data}) => {
+    commit('ADD_USER', data)
+    router.push({name: 'Home'})
+  })
 }
-
+export const getUser = ({commit, getters}) => {
+  getters.isOnline ? router.push({name: 'Home'}) : router.push({name: 'Login'})
+}
+export const getUsers = ({commit, getters}) => {
+  return ApiServices.users().then(({data}) => {
+    commit('ADD_USERS', data)
+  })
+}
 export const editUser = ({dispatch, commit, state}) => {
-  Api.editUser(state.user._id).then(({data}) => {
-    commit('EDIT', data)
+  ApiServices.editUser(state.user._id).then(({data}) => {
+    commit('EDIT_USER', data)
   })
 }
-
 export const updateUser = ({dispatch, commit, state}, form) => {
-  Api.updateUser(form).then(({data}) => {
-    commit('ADD', data)
+  ApiServices.updateUser(form).then(({data}) => {
+    commit('ADD_USER', data)
   })
 }
+export const newDependence = ({dispatch, commit, state}, form) => {
+  let formData = new FormData()
+  formData.append('name', form.name)
+  formData.append('acronym', form.acronym)
+  formData.append('annex', form.annex)
+  formData.append('latitude', form.latitude)
+  formData.append('longitude', form.longitude)
+  formData.append('photo', form.photo)
 
+  ApiServices.newDependence(formData).then(({data}) => {
+    commit('ADD_DEPENDENCE', data)
+  })
+}
+export const editDependence = ({dispatch, commit, state}, id, index) => {
+  ApiServices.editDependence(id).then(({data}) => {
+    commit('EDIT_DEPENDENCE', data, index)
+  })
+}
+export const updateDependence = ({dispatch, commit, state}, form, index) => {
+  ApiServices.updateDependence(form).then(({data}) => {
+    commit('UPDATE_DEPENDENCE', data, index)
+  })
+}
+export const getDependences = ({dispatch, commit, state}) => {
+  ApiServices.getDependences().then(({data}) => {
+    commit('GET_DEPENDENCES', data, { root: true })
+  })
+}
 export const login = ({dispatch, commit, state}, form) => {
-  return LocalServices.login(form).then(({data}) => {
-    commit('ADD', data)
+  return ApiServices.login(form).then(({data}) => {
+    commit('ADD_USER', data)
+  }).then(() => {
+    router.push({name: 'Home'})
   })
 }
-
 export const logout = ({dispatch, commit, state}) => {
   commit('LOGOUT')
+  router.push({name: 'Login'})
+}
+export const addDatatable = ({dispatch, commit, state}, data) => {
+  commit('ADD_DATATABLE', data)
+}
+export const getSolicitudes = ({commit, getters}) => {
+  commit('GET_SOLICITUDES')
 }
