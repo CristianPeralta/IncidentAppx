3d6e2986a04bd55be03875452c22f2f251a29a2a
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-20 22:03:14 -0500

    use forms to re-use components

--- src/components/NewDependence.vue
+++ src/components/NewDependence.vue
@@ -5,56 +5,7 @@
         <div class="container has-text-centered">
           <div class="column is-4 is-offset-4">
             <h3 class="title has-text-grey" style="margin-top : 40px;" >New Dependence</h3>
-              <div class="box">
-                  <figure class="avatar">
-                    <img height="128" width="128" :src='preview'>
-                    <div class="file is-centered">
-                      <label class="file-label">
-                        <input class="file-input" type="file" name="photo" @change="processFile($event)">
-                        <span class="file-cta">
-                          <span class="file-icon">
-                            <i class="fas fa-upload"></i>
-                          </span>
-                          <span class="file-label">
-                            Choose a file…
-                          </span>
-                        </span>
-                      </label>
-                    </div>
-                  </figure>
-
-                  <div class="field">
-                    <div class="control">
-                      <input class="input is-large" type="text" name="name" v-model="form.name" placeholder="Name" autofocus="">
-                    </div>
-                  </div>
-
-                  <div class="field">
-                    <div class="control">
-                      <input class="input is-large" type="text" name="acronym" v-model="form.acronym" placeholder="Acronym">
-                    </div>
-                  </div>
-
-                  <div class="field">
-                    <div class="control">
-                      <input class="input is-large" type="text" name="annex" v-model="form.annex" placeholder="Annex">
-                    </div>
-                  </div>
-
-                  <div class="field">
-                    <div class="control">
-                      <input class="input is-large" type="text" name="latitude" v-model="form.latitude" placeholder="Latitude">
-                    </div>
-                  </div>
-
-                  <div class="field">
-                    <div class="control">
-                      <input class="input is-large" type="text" name="longitude" v-model="form.longitude" placeholder="Longitude">
-                    </div>
-                  </div>
-
-                  <button @click="sendForm()" class="button is-block is-info is-large is-fullwidth">Add</button>
-              </div>
+              <DependenceForm></DependenceForm>
           </div>
         </div>
       </div>
@@ -63,6 +14,8 @@
 </template>
 
 <script>
+import DependenceForm from './Forms/DependenceForm'
+
 export default {
   name: 'NewDependence',
   data () {
@@ -78,6 +31,9 @@ export default {
       allDependences: [],
       preview: 'https://st3.depositphotos.com/5266903/12981/v/950/depositphotos_129812138-stock-illustration-management-office-flat-vector-icon.jpg'
     }
+  },
+  components: {
+    DependenceForm
   },
   created () {
     this.getDependences()
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,62 +4,7 @@
       <div class="container has-text-centered">
         <div class="column is-4 is-offset-4">
           <h3 class="title has-text-grey">Sign Up</h3>
-          <div class="box">
-              <figure class="avatar">
-               <img height="128" width="128" src='https://image.flaticon.com/icons/svg/179/179948.svg'>
-             </figure>
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="text" v-model="form.name" placeholder="Your Name" autofocus="">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="text" v-model="form.lastname" placeholder="Your Lastname">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="text" v-model="form.dni" placeholder="Your DNI">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="text" v-model="form.cellphone" placeholder="Your cellphone">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="email" v-model="form.email" placeholder="Your Email">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <select v-model="form.dependence" class="input select" placeholder="dependence">
-                    <option v-for="(dependence, index) in allDependences" :value="dependence._id" :key="index">{{dependence.acronym}}</option>
-                  </select>
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="password" v-model="form.password" placeholder="Your Password">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="password" v-model="passwordDraft" placeholder="Confirm Password">
-                </div>
-              </div>
-
-              <button @click="sendForm()" class="button is-block is-info is-large is-fullwidth">Sign Me Up</button>
-          </div>
+            <UserForm></UserForm>
           <p class="has-text-grey">
             <router-link to="/" exact>
               <a>Sign In</a> &nbsp;·&nbsp;
@@ -72,6 +17,7 @@
 </template>
 
 <script>
+import UserForm from './Forms/UserForm'
 export default {
   name: 'SignUp',
   data () {
@@ -87,6 +33,9 @@ export default {
       },
       passwordDraft: ''
     }
+  },
+  components: {
+    UserForm
   },
   created () {
     this.getDependences()
