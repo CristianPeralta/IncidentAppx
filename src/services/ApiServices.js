110495f7c6c4b78048b672bf03931599b9bf1abc
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-25 07:22:25 -0500

    add services to manage incidents

--- src/services/ApiServices.js
+++ src/services/ApiServices.js
@@ -16,6 +16,12 @@ export default {
   logout () {
     return Api().post('logout')
   },
+  editUser (id, token) {
+    return Api().get(`users/${id}`, token)
+  },
+  updateUser (id, params, token) {
+    return Api().put(`users/${id}`, params, token)
+  },
   newDependence (params, token) {
     return Api().post('dependences', params, {
       headers: {
@@ -27,16 +33,27 @@ export default {
   getDependences () {
     return Api().get('dependences')
   },
-  editUser (id, token) {
-    return Api().get(`users/${id}`, token)
-  },
-  updateUser (id, params, token) {
-    return Api().put(`users/${id}`, params, token)
-  },
   editDependence (id, token) {
     return Api().get(`dependences/${id}`, token)
   },
   updateDependence (id, params, token) {
     return Api().put(`dependences/${id}`, params, token)
+  },
+  newIncident (params, token) {
+    return Api().post('incidents', params, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'x-access-token': localStorage.getItem('token')
+      }
+    })
+  },
+  getIncidents () {
+    return Api().get('incidents')
+  },
+  editIncident (id, token) {
+    return Api().get(`incidents/${id}`, token)
+  },
+  updateIncident (id, params, token) {
+    return Api().put(`incidents/${id}`, params, token)
   }
 }
