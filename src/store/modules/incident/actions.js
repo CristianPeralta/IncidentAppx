25b7306eed4791c454c0c2dad9fd6b8d9d795e54
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-30 08:41:12 -0500

    add action to edit and update incidents

--- src/store/modules/incident/actions.js
+++ src/store/modules/incident/actions.js
@@ -11,3 +11,15 @@ export const getIncidents = ({dispatch, commit, state}) => {
     commit('ADD_INCIDENTS', data)
   })
 }
+
+export const editIncident = ({dispatch, commit, state}, id, index) => {
+  return Api.editIncident(id).then(({data}) => {
+    commit('EDIT_INCIDENT', data, index)
+  })
+}
+
+export const updateIncident = ({dispatch, commit, state}, form) => {
+  Api.updateIncident(form).then(({data}) => {
+    commit('UPDATE_INCIDENT', data, state.incidentIndex)
+  })
+}
