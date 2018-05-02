4a3b85b41e33373e57d8b0ef49d259171468090b
CristianPeralta <cbperaltasegura@gmail.com>
2018-05-02 05:20:43 -0500

    add complete uri to get photo of dependence

--- src/components/Forms/DependenceForm.vue
+++ src/components/Forms/DependenceForm.vue
@@ -100,7 +100,7 @@ export default {
         this.$store.dispatch('dependence/editDependence', this.data, this.index).then(() => {
           if (this.dependence) {
             this.form = this.dependence
-            this.preview = this.dependence.photo
+            this.preview = 'http://localhost:3000' + this.dependence.photo.substring(6, this.dependence.photo.length)
           }
         })
       } else {
