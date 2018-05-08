91b09727111f5b5ecb4ddbf4c4e5e9f80f0d5636
CristianPeralta <cbperaltasegura@gmail.com>
2018-05-08 05:54:54 -0500

    add solicitudes view component

---
+++ src/components/SolicitudesView.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <Section v-if="datatable == 'solicitudes'" :columnsD="['name', 'email', 'role', 'actions']" :dataD="allSolicitudes" :optionsD="{
+    headings: {
+      'name': 'Name',
+      'email': 'Email',
+      'role': 'Role'
+    },
+    sortable: ['name', 'email', 'role'],
+    filterable: ['name', 'email', 'role']
+  }"></Section>
+</template>
+
+<script>
+import Section from './Section'
+export default {
+  name: 'SolicitudesView'
+  components: {
+    Section
+  }
+}
+</script>
+
+<style lang="css">
+</style>
