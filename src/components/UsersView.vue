4b98e6660056522e12309d2a3738832cffcecc61
CristianPeralta <cbperaltasegura@gmail.com>
2018-05-08 05:52:53 -0500

    add usersview component

---
+++ src/components/UsersView.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <Section v-if="datatable == 'users'" :columnsD="['name', 'email', 'dependence', 'role', 'actions']" :dataD="allUsers" :optionsD="{
+    headings: {
+      'name': 'Name',
+      'email': 'Email',
+      'dependence': 'Dependence',
+      'role': 'Role'
+    },
+    sortable: ['name', 'email', 'role'],
+    filterable: ['name', 'email', 'role']
+  }">
+</template>
+
+<script>
+import Section from './Section'
+export default {
+  name: 'UsersView'
+  components: {
+    Section
+  }
+}
+</script>
+
+<style lang="css">
+</style>
