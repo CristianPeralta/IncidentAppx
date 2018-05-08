e5944f010c3ddef0b539b2fb23ba1c9a7b4a1f39
CristianPeralta <cbperaltasegura@gmail.com>
2018-05-08 05:57:00 -0500

    add dependences view component

---
+++ src/components/DependencesView.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <Section v-if="datatable == 'dependences'" :columnsD="['name', 'acronym', 'annex', 'actions']" :dataD="allDependences" :optionsD="{
+    headings: {
+      'name': 'Name',
+      'acronym': 'Acronym',
+      'annex': 'Annex'
+    },
+    sortable: ['name', 'acronym', 'annex'],
+    filterable: ['name', 'acronym', 'annex']
+  }"></Section>
+</template>
+
+<script>
+import Section from './Section'
+export default {
+  name: 'DependencesView'
+  components: {
+    Section
+  }
+}
+</script>
+
+<style lang="css">
+</style>
